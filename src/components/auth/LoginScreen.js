b8f5a6d932d0a8763e5731098f462aef41b26778
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2022-04-22 23:59:53 -0400

    Authenticacion de Login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from '../..//hooks/useForm'
+import { useDispatch } from 'react-redux'
+import { starLoginEmailPassword, startGoogleLogin } from '../../actions/auth'
 export const LoginScreen = () => {
+
+   const dispatch = useDispatch();
+
+   const [formValues, handleInputChange] = useForm({
+      email: 'nando@gmail.com',
+      password: '1234'
+   })
+
+   const { email, password } = formValues;
+
+   const handleLogin = (event) => {
+      event.preventDefault();
+      dispatch(starLoginEmailPassword(email, password));
+   }
+
+   const handleGoogleLogin = () => {
+      dispatch(startGoogleLogin());
+   }
    return (
       <>
          <h3 className='auth__title'>Login</h3>
-         <form>
+         <form onSubmit={handleLogin}>
             <input
                type="text"
                placeholder='email'
                name='email'
                className='auth__input'
                autoComplete='off'
+               value={email}
+               onChange={handleInputChange}
             />
             <input
                type="password"
@@ -18,6 +41,8 @@ export const LoginScreen = () => {
                name='password'
                className='auth__input'
                autoComplete='off'
+               value={password}
+               onChange={handleInputChange}
             />
             <button
                type='submit'
@@ -28,12 +53,12 @@ export const LoginScreen = () => {
             </button>
             <div className='auth__social-networks mb-5'>
                <p>Login with Social Networks</p>
-               <div className='google-btn'>
+               <div className='google-btn' onClick={handleGoogleLogin}>
                   <div className='google-icon-wrapper'>
                      <img className='google-icon' src='https://cdn-icons-png.flaticon.com/512/2965/2965278.png' alt='google-icon' />
                   </div>
                   <p className='btn-text'>
-                     <p>Sign in with google</p>
+                     Sign in with google
                   </p>
                </div>
             </div>
